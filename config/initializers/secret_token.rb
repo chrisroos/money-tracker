82d59a7fc90e4134beb9d110f2e89cdf8e35facf
Chris Roos <chris@seagul.co.uk>
2010-09-25 14:40:27 +0100

    Empty rails 3 app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoneyTracker::Application.config.secret_token = 'a70b5afbc0dd0fa89c8d82f5748dd876632728c48f692eaa9544a04a53de96b06fcacef1f436747a98b7ff298b1abdb8c255c766645ea68f107c532dd7695a26'
