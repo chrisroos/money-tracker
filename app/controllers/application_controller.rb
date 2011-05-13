dfb3307c465fe0a00db0348b7e929e0108ae6f5c
Chris Roos <chris@seagul.co.uk>
2011-05-13 15:56:49 +0100

    Added production only HTTP auth to the app.
    I want to deploy to Heroku and want some basic HTTP auth to protect my data.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
+  
+  before_filter :authenticate
+  
   protect_from_forgery
+  
+  private
+  
+  def authenticate
+    return true unless Rails.env.production?
+    
+    authenticate_or_request_with_http_basic do |user_name, password|
+      user_name == USERNAME && password == PASSWORD
+    end
+  end
+  
 end
