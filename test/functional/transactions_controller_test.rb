940c276d15b9fd8135f7d08629a9e8f714e44151
Chris Roos <chris@seagul.co.uk>
2011-05-18 17:29:58 +0100

    Add explicit tests around (buggy?) behaviour introduced in Rails 3.0.2.
    I'm yet to find the commit that introduced it but Rails 3.0.2 changes the behaviour of value_before_type_cast (see https://github.com/rails/rails/blob/v3.0.2/actionpack/lib/action_view/helpers/form_helper.rb#L1015) so that it tries to load the value from the database in favour of the value from the object instance.  This causes a problem with my methods (#date and #description) that are database columns that also have default values.
    
    I plan to find the actual Rails commit and work out whether it was an intentional behaviour change.
    
    For now, I've added the relevant duplication to ensure the form does the right thing and the tests pass.

--- test/functional/transactions_controller_test.rb
+++ test/functional/transactions_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
-class TransactionsControllerTest < ActionController::TestCase
+class TransactionsControllerIndexTest < ActionController::TestCase
+  tests TransactionsController
   
   should "redirect to the most recent period" do
     current_period = Date.today.to_s(:period)
@@ -8,6 +9,38 @@ class TransactionsControllerTest < ActionController::TestCase
     get :index
     
     assert_redirected_to transactions_path(:period => current_period)
+  end
+
+end
+
+class TransactionsControllerEditTest < ActionController::TestCase
+  tests TransactionsController
+  
+  should "display original values in the edit form" do
+    date   = Date.parse('2011-01-01')
+    period = date.to_s(:period)
+    transaction = Factory.create(:transaction, :name => 'name', :memo => 'memo', :type => 'type', :original_date => date)
+    
+    get :index, :period => period, :edit => true
+
+    assert_equal date, transaction.date
+    assert_equal 'name / memo (type)', transaction.description
+    
+    assert_select "input[name='transaction[date]'][value='#{date}']"
+    assert_select "input[name='transaction[description]'][value='name / memo (type)']"
+    assert_select "textarea[name='transaction[note]']", :text => ''
+  end
+  
+  should "display user-entered values in the edit form" do
+    date   = Date.parse('2011-01-01')
+    period = date.to_s(:period)
+    Factory.create :transaction, :description => 'custom-description', :date => date, :note => 'custom-note'
+    
+    get :index, :period => period, :edit => true
+  
+    assert_select "input[name='transaction[date]'][value='#{date}']"
+    assert_select "input[name='transaction[description]'][value='custom-description']"
+    assert_select "textarea[name='transaction[note]']", :text => 'custom-note'
   end
   
 end
