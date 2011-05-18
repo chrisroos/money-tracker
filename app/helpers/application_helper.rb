af1512dd396ed711eb5d08e4c8f4471492109b3c
Chris Roos <chris@seagul.co.uk>
2011-05-18 14:35:30 +0100

    DRYed up the use of more magic date format strings.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,12 @@ module ApplicationHelper
   
   def link_to_previous_period(period)
     previous_period = Date.parse("#{period}-01") - 1.month
-    link_to previous_period.strftime("%B %Y"), transactions_path(:period => previous_period.to_s(:period))
+    link_to previous_period.to_s(:month_and_year), transactions_path(:period => previous_period.to_s(:period))
   end
   
   def link_to_next_period(period)
     next_period = Date.parse("#{period}-01") + 1.month
-    link_to next_period.strftime("%B %Y"), transactions_path(:period => next_period.to_s(:period))
+    link_to next_period.to_s(:month_and_year), transactions_path(:period => next_period.to_s(:period))
   end
   
 end
