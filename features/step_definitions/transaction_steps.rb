e0ec41cfb077a0a8a5576672f8277b02b926b3ca
Chris Roos <chris@seagul.co.uk>
2011-06-07 17:09:18 +0100

    Display the income and expenditure totals at the bottom of the list of transactions.
    These totals feel better placed at the bottom of the table of transactions, rather than at the top of the page as they were previously.
    
    *NOTE* I've had to tell cucumber not to raise an exception on surplus rows (':surplus_rows => false') so that the table diffing in the cucumber features continues to work.

--- features/step_definitions/transaction_steps.rb
+++ features/step_definitions/transaction_steps.rb
@@ -1,5 +1,5 @@
 Then /^I should see the following transactions:$/ do |expected_transactions_table|
-  expected_transactions_table.diff!(tableish('table tr', 'td,th'))
+  expected_transactions_table.diff!(tableish('table tr', 'td,th'), :surplus_row => false)
 end
 
 Then /^I should see that the income of the transactions is £(\d+)$/ do |income|
