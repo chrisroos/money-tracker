48a9d1afe41d7125cbb76834f52b1963fe8eed6e
Chris Roos <chris@seagul.co.uk>
2011-06-08 15:15:57 +0100

    Introduce within_transaction to DRY up the transaction_helper a little.

--- features/helpers/transaction_helper.rb
+++ features/helpers/transaction_helper.rb
@@ -1,6 +1,5 @@
 def assert_credit_transaction(amount, date, description, note = nil, category = nil)
-  transaction = Transaction.find_by_description(description) || Transaction.find_by_original_description(description)
-  within "#transaction_#{transaction.id}" do
+  within_transaction(description) do
     assert page.has_css?('.paid_in', :text => amount)
     assert page.has_css?('.date', :text => Date.parse(date).to_s(:weekday_and_day))
     assert page.has_css?('.description', :text => description)
@@ -10,12 +9,16 @@ def assert_credit_transaction(amount, date, description, note = nil, category = 
 end
 
 def assert_debit_transaction(amount, date, description, note = nil, category = nil)
-  transaction = Transaction.find_by_description(description) || Transaction.find_by_original_description(description)
-  within "#transaction_#{transaction.id}" do
+  within_transaction(description) do
     assert page.has_css?('.paid_out', :text => amount)
     assert page.has_css?('.date', :text => Date.parse(date).to_s(:weekday_and_day))
     assert page.has_css?('.description', :text => description)
     assert page.has_css?('.note', :text => note) if note
     assert page.has_css?('.category', :text => category) if category
   end
+end
+
+def within_transaction(description, &blk)
+  transaction = Transaction.find_by_description(description) || Transaction.find_by_original_description(description)
+  within "#transaction_#{transaction.id}", &blk
 end
