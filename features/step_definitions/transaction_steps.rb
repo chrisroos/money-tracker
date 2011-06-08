12166bce4ae2aff47a11ca6738b3ca3e6b7fbc51
Chris Roos <chris@seagul.co.uk>
2011-06-08 15:11:45 +0100

    Remove a step made redundant by aa319021b48cc96a71db774f28bc0ebad02793b1.

--- features/step_definitions/transaction_steps.rb
+++ features/step_definitions/transaction_steps.rb
@@ -1,7 +1,3 @@
-Then /^I should see the following transactions:$/ do |expected_transactions_table|
-  expected_transactions_table.diff!(tableish('table tr', 'td,th'), :surplus_row => false)
-end
-
 Then /^I should see that the income of the transactions is £(\d+)$/ do |income|
   assert page.has_css?('#income', :text => "£#{income}")
 end
