febcb2797e8d09408c591be32dffe7e584ff5407
Chris Roos <chris@seagul.co.uk>
2011-08-01 17:07:13 +0100

    Extracted some of the duplication in the transaction_helper.

--- features/helpers/transaction_helper.rb
+++ features/helpers/transaction_helper.rb
@@ -1,31 +1,47 @@
 def assert_credit_transaction(amount, date, description, note = nil, category = nil)
-  xpath_transaction = "*[@class='transaction']"
-  xpath_paid_out    = "*[@class='paid_in'][contains(text(), '#{amount}')]"
-  xpath_description = "*[@class='description']/a[contains(text(), '#{description}')]"
-  xpath_date        = "*[contains(@class, 'date')][contains(text(), '#{Date.parse(date).to_s(:weekday_and_day)}')]"
-  xpath_note        = "*[@class='note'][contains(text(), '#{note}')]"
-  xpath_category    = "*[@class='category']/a[contains(text(), '#{category}')]"
-  xpath = "//#{xpath_transaction}//#{xpath_paid_out}"
-  xpath += "/ancestor::#{xpath_transaction}//#{xpath_description}"
-  xpath += "/ancestor::#{xpath_transaction}//#{xpath_date}"
-  xpath += "/ancestor::#{xpath_transaction}//#{xpath_note}" if note
-  xpath += "/ancestor::#{xpath_transaction}//#{xpath_category}" if category
+  xpath = "//#{xpath_transaction}//#{xpath_paid_in(amount)}"
+  xpath += "/ancestor::#{xpath_transaction}//#{xpath_description(description)}"
+  xpath += "/ancestor::#{xpath_transaction}//#{xpath_date(date)}"
+  xpath += "/ancestor::#{xpath_transaction}//#{xpath_note(note)}" if note
+  xpath += "/ancestor::#{xpath_transaction}//#{xpath_category(category)}" if category
 
   assert page.has_xpath?(xpath)
 end
 
 def assert_debit_transaction(amount, date, description, note = nil, category = nil)
-  xpath_transaction = "*[@class='transaction']"
-  xpath_paid_out    = "*[@class='paid_out'][contains(text(), '#{amount}')]"
-  xpath_description = "*[@class='description']/a[contains(text(), '#{description}')]"
-  xpath_date        = "*[contains(@class, 'date')][contains(text(), '#{Date.parse(date).to_s(:weekday_and_day)}')]"
-  xpath_note        = "*[@class='note'][contains(text(), '#{note}')]"
-  xpath_category    = "*[@class='category']/a[contains(text(), '#{category}')]"
-  xpath = "//#{xpath_transaction}//#{xpath_paid_out}"
-  xpath += "/ancestor::#{xpath_transaction}//#{xpath_description}"
-  xpath += "/ancestor::#{xpath_transaction}//#{xpath_date}"
-  xpath += "/ancestor::#{xpath_transaction}//#{xpath_note}" if note
-  xpath += "/ancestor::#{xpath_transaction}//#{xpath_category}" if category
+  xpath = "//#{xpath_transaction}//#{xpath_paid_out(amount)}"
+  xpath += "/ancestor::#{xpath_transaction}//#{xpath_description(description)}"
+  xpath += "/ancestor::#{xpath_transaction}//#{xpath_date(date)}"
+  xpath += "/ancestor::#{xpath_transaction}//#{xpath_note(note)}" if note
+  xpath += "/ancestor::#{xpath_transaction}//#{xpath_category(category)}" if category
 
   assert page.has_xpath?(xpath)
+end
+
+def xpath_transaction
+  "*[@class='transaction']"
+end
+
+def xpath_paid_in(amount)
+  "*[@class='paid_in'][contains(text(), '#{amount}')]"
+end
+
+def xpath_paid_out(amount)
+  "*[@class='paid_out'][contains(text(), '#{amount}')]"
+end
+
+def xpath_description(description)
+  "*[@class='description']/a[contains(text(), '#{description}')]"
+end
+
+def xpath_date(date)
+  "*[contains(@class, 'date')][contains(text(), '#{Date.parse(date).to_s(:weekday_and_day)}')]"
+end
+
+def xpath_note(note)
+  "*[@class='note'][contains(text(), '#{note}')]"
+end
+
+def xpath_category(category)
+  "*[@class='category']/a[contains(text(), '#{category}')]"
 end
