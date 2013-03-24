96151d4476485b016d8b2ae329b3c982eb20d190
Chris Roos <chris@seagul.co.uk>
2013-03-24 14:17:17 +0000

    Use asset pipeline
    * Enabled assets in application.rb
    
    * Moved styles in public/stylesheets/money_tracker.css to
    app/assets/stylesheets/application.css
    
    * Moved javascript in public/javascripts/application.js to
    app/assets/javascripts/application.js
    
    * Updated application layout to pick up assets files
    
    * Removed outdated jquery libraries
    
    * Removed app/assets/images/rails.png
    
    * Removed public/images/rails.png

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,28 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(document).ready(function() {
+  $('#transactions form').submit(function() {
+    var form = $(this);
+    var data = form.serialize();
+    var url  = form.attr('action');
+    $.post(url, data, function(data) {
+      $('.edited', form).animate({backgroundColor: '#fff'}, {
+        duration: 1500,
+        complete: function() {
+          $(this).removeClass('edited');
+          $(this).css('backgroundColor', '');
+        }
+      });
+    });
+    return false;
+  });
+
+  $('#transactions input[type=text], #transactions textarea').blur(function() {
+    $(this).addClass('edited');
+    $(this).parent('form').submit();
+  });
+
+  $('#transactions input[type=submit]').remove();
+});
