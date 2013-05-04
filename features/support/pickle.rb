6b75f6dcd076cd5cd6c09da4aefdd9ab5461a75a
Chris Roos <chris@seagul.co.uk>
2013-05-04 13:42:13 +0100

    Remove non-ascii characters from comments

--- features/support/pickle.rb
+++ features/support/pickle.rb
@@ -6,9 +6,9 @@
 #
 # For machinist add: features/support/machinist.rb
 #
-#    require 'machinist/active_record' # or your chosen adaptor
+#    require 'machinist/active_record' # or your chosen adaptor
 #    require File.dirname(__FILE__) + '/../../spec/blueprints' # or wherever your blueprints are
-#    Before { Sham.reset } # to reset Sham's seed between scenarios so each run has same random sequences
+#    Before { Sham.reset } # to reset Sham's seed between scenarios so each run has same random sequences
 #
 # For FactoryGirl add: features/support/factory_girl.rb
 #
