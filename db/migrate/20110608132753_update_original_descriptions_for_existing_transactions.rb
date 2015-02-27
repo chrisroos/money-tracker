69675584cab5d2fe1268d0ae1da0bc98ccd7669f
Chris Roos <chris@seagul.co.uk>
2015-02-27 14:52:39 +0000

    Fix Style/SpaceInsideHashLiteralBraces violations

--- db/migrate/20110608132753_update_original_descriptions_for_existing_transactions.rb
+++ db/migrate/20110608132753_update_original_descriptions_for_existing_transactions.rb
@@ -1,6 +1,6 @@
 class UpdateOriginalDescriptionsForExistingTransactions < ActiveRecord::Migration
   def self.up
-    Transaction.find_each(conditions: {original_description: nil}) do |transaction|
+    Transaction.find_each(conditions: { original_description: nil }) do |transaction|
       transaction.send :set_original_description
       transaction.save!
     end
