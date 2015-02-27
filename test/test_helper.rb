3d2217fd3c9d94d378afd2c8bcb46e7bede4a13d
Chris Roos <chris@seagul.co.uk>
2015-02-27 17:14:40 +0000

    Fix Style/ClassAndModuleChildren violations
    I wasn't sure whether to make this change as test_helper.rb is a Rails
    generated file. In the end I figured I'd go with it as this is the last
    violation that needs fixed.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,21 +2,23 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
-class ActiveSupport::TestCase
-  ActiveRecord::Migration.check_pending!
+module ActiveSupport
+  class TestCase
+    ActiveRecord::Migration.check_pending!
 
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  #
-  # Note: You'll currently still have to declare fixtures explicitly in integration tests
-  # -- they do not yet inherit this setting
-  fixtures :all
+    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+    #
+    # Note: You'll currently still have to declare fixtures explicitly in integration tests
+    # -- they do not yet inherit this setting
+    fixtures :all
 
-  # Add more helper methods to be used by all tests here...
-  def in_demo_mode
-    ENV['DEMO_MODE'] = 'true'
-    yield
-  ensure
-    ENV.delete('DEMO_MODE')
+    # Add more helper methods to be used by all tests here...
+    def in_demo_mode
+      ENV['DEMO_MODE'] = 'true'
+      yield
+    ensure
+      ENV.delete('DEMO_MODE')
+    end
   end
 end
 
