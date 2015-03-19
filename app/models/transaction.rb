201a4f2afda42f85511a8eb43d602716d76a05d1
Chris Roos <chris@seagul.co.uk>
2015-03-19 17:02:56 +0000

    Avoid repetition of `self.`
    By grouping class methods in a `class << self` block.

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -8,37 +8,39 @@ class Transaction < ActiveRecord::Base
 
   default_scope { order('COALESCE(date, source_date) DESC') }
 
-  def self.search(search_string)
-    if (m = /category:(.*)/.match(search_string))
-      where(category: m[1])
-    elsif (m = /description:(.*)/.match(search_string))
-      where(description: m[1])
-    else
+  class << self
+    def search(search_string)
+      if (m = /category:(.*)/.match(search_string))
+        where(category: m[1])
+      elsif (m = /description:(.*)/.match(search_string))
+        where(description: m[1])
+      else
+        where(
+          'source_name ILIKE :q OR source_memo ILIKE :q OR note ILIKE :q OR source_type ILIKE :q OR description ILIKE :q',
+          q: "%#{search_string}%"
+        )
+      end
+    end
+
+    def period(period)
+      date = Date.from_period(period)
       where(
-        'source_name ILIKE :q OR source_memo ILIKE :q OR note ILIKE :q OR source_type ILIKE :q OR description ILIKE :q',
-        q: "%#{search_string}%"
+        'COALESCE(date, source_date) >= ? AND COALESCE(date, source_date) <= ?',
+        date.beginning_of_month, date.end_of_month
       )
     end
-  end
 
-  def self.period(period)
-    date = Date.from_period(period)
-    where(
-      'COALESCE(date, source_date) >= ? AND COALESCE(date, source_date) <= ?',
-      date.beginning_of_month, date.end_of_month
-    )
-  end
+    def category_search(query)
+      unscoped.select('DISTINCT(category)').where('category ILIKE :q', q: "%#{query}%").order('category ASC')
+    end
 
-  def self.category_search(query)
-    unscoped.select('DISTINCT(category)').where('category ILIKE :q', q: "%#{query}%").order('category ASC')
-  end
+    def description_search(query)
+      unscoped.select('DISTINCT(description)').where('description ILIKE :q', q: "%#{query}%").order('description ASC')
+    end
 
-  def self.description_search(query)
-    unscoped.select('DISTINCT(description)').where('description ILIKE :q', q: "%#{query}%").order('description ASC')
-  end
-
-  def self.location_search(description, query)
-    unscoped.select('DISTINCT(location)').where('UPPER(description) = UPPER(:description) AND location ILIKE :q', description: description, q: "%#{query}%").order('location ASC')
+    def location_search(description, query)
+      unscoped.select('DISTINCT(location)').where('UPPER(description) = UPPER(:description) AND location ILIKE :q', description: description, q: "%#{query}%").order('location ASC')
+    end
   end
 
   def amount
