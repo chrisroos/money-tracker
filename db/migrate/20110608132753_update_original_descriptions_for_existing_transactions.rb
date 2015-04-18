c6b80398ce6e12899ed96c7373442a7df47be8ff
Chris Roos <chris@seagul.co.uk>
2015-04-18 20:25:17 +0100

    Fix migration
    Passing a hash of `conditions` to `find_each` is no longer supported.
    
    I ran into this problem when trying to run migrations from scratch, to see
    whether the schema had changed with a newer version of Rails.

--- db/migrate/20110608132753_update_original_descriptions_for_existing_transactions.rb
+++ db/migrate/20110608132753_update_original_descriptions_for_existing_transactions.rb
@@ -1,6 +1,6 @@
 class UpdateOriginalDescriptionsForExistingTransactions < ActiveRecord::Migration
   def self.up
-    Transaction.find_each(conditions: { original_description: nil }) do |transaction|
+    Transaction.where(original_description: nil).find_each do |transaction|
       transaction.send :set_original_description
       transaction.save!
     end
