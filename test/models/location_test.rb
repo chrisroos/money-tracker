8e4e06e476b1f9d5b0eeebd7a721c8241dad8310
Chris Roos <chris@seagul.co.uk>
2015-07-18 15:16:31 +0100

    Fix ParallelAssignment Rubocop violation
    http://www.rubydoc.info/gems/rubocop/0.31.0/RuboCop/Cop/Performance/ParallelAssignment

--- test/models/location_test.rb
+++ test/models/location_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class LocationTest < ActiveSupport::TestCase
   test 'should return an array of locations for the given description' do
-    description, query = 'description', 'query-string'
+    description = 'description'
+    query = 'query-string'
     Transaction.stubs(:location_search).with(description, query).returns([stub(location: 'location-name')])
     assert_equal ['location-name'], Location.search(description, query)
   end
